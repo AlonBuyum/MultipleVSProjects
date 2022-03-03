3c12b80358d771b73fdfa4c8ea188b7ea9d8ee9e
AlonBuyum <alon_buyum@hotmail.com>
2022-03-03 17:21:38 +0200

    Add project files.

---
+++ CSharpApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello CSharpApp2");
+        }
+    }
+}
